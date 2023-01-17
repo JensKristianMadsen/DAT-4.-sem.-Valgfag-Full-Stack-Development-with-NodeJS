f3414006ce9fccb460e874f65df593c781b80386
Jens-k-m-m <jensmonteiro@gmail.com>
2023-01-17 13:55:30 +0100

    set up Redux and Toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"; // This is so we can save all the state so in(index.js state) all of this information will be stored in local state meaning! anytime the user like closes the tab or closes the browser that information will still be stored in there the user information, and they can get rid of it is if they clear their cache. But storage is an alternative that will have information saved for thar particular session but if you close the tab that information will go away.
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({ //https://redux-toolkit.js.org/usage/usage-guide   store = configureStore
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+
   </React.StrictMode>
 );
